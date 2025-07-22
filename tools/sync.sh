9282f1ebd3f4542a65d37d19ae230714db852781
yao-xiaobai <17625331900@163.com>
2025-07-22 20:10:52 +0800

    opt sync.sh

--- tools/sync.sh
+++ tools/sync.sh
@@ -11,8 +11,8 @@ mkdir -p "${work_dir}" && cd "${work_dir}" || exit
 
 # 下载更新代码
 echo "git clone/pull --mirror ${gitcode_url}"
-if [ -d "${repo}" ]; then
-  cd "${repo}" || exit
+if [ -d "${repo}.git" ]; then
+  cd "${repo}.git" || exit
   git remote set-url origin "${gitcode_url}"    # 将代码仓远程地址设置成gitcode地址
   git pull --mirror "${gitcode_url}"
 else
@@ -22,8 +22,8 @@ fi
 
 # 推送代码
 echo "git push --mirror ${mirror_url}"
-if [ -d "${repo}" ]; then
-  cd "${repo}" || exit
+if [ -d "${repo}.git" ]; then
+  cd "${repo}.git" || exit
   git remote set-url origin "${mirror_url}"    # 将代码仓远程地址设置成gitcode地址
-  git push --mirror "${mirror_url}"
+  git push "${mirror_url} --prune --force '+refs/heads/*:refs/heads/*' '+refs/tags/*:refs/tags/*"
 fi
