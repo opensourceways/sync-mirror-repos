1e66f0f71d0e90f687088c542551513f367bd6d9
yao-xiaobai <17625331900@163.com>
2025-07-23 15:37:23 +0800

    update sync.sh

--- tools/sync.sh
+++ tools/sync.sh
@@ -15,7 +15,7 @@ if [ -d "${repo}.git" ]; then
   cd "${repo}.git" || exit
   git remote set-url origin "${gitcode_url}"    # 将代码仓远程地址设置成gitcode地址
   echo "[${repo}] git pull --mirror ${gitcode_url}"
-  git fetch --all --prune "${gitcode_url}"
+  git fetch --all --prune
 else
   echo "[${repo}] git clone --mirror ${gitcode_url}"
   git clone --mirror "${gitcode_url}"
